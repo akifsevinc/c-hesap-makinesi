e33967108181ad0b828ac4ac93df253cdea8b03d
akifsevinc <akifsevinc@proton.me>
2026-06-25 18:22:32 +0300

    Fix main function declaration and improve division check

--- hesapmakinesi.c
+++ hesapmakinesi.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
+int main(){
 int secim;
 int sayi1;
 int sayi2;
-
-int main(){
 	printf("Lütfen yapmak istediğiniz işlemi giriniz:\n");
 	printf("1. toplama\n");
 	printf("2. çıkarma\n");
 	printf("3. çarpma\n");
-    printf("4. bölme\n");
+	printf("4. bölme\n");
 	scanf("%d", &secim);
 	printf("Sayı giriniz:");
 	scanf("%d", &sayi1);
@@ -26,7 +25,7 @@ int main(){
 	printf("Sonuç:%d\n", sayi1*sayi2);
 }
 	else if (secim == 4){
-	if (sayi1, sayi2 == 0){
+	if (sayi2 == 0){
 	printf("Sıfır ile bölme yapamazsın!\n");
 	return 0;
 }
